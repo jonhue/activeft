4842b0c39fb81b967977d9af7bfed54323e95f57
Jonas Hübotter <jonas.huebotter@gmail.com>
2025-08-28 20:32:24 +0200

    make faiss import optional

--- activeft/__init__.py
+++ activeft/__init__.py
@@ -158,7 +158,11 @@ If you use the code in a publication, please cite our papers:
 """
 
 from activeft.active_data_loader import ActiveDataLoader
-from activeft import acquisition_functions, data, embeddings, model, sift
+from activeft import acquisition_functions, data, embeddings, model
+try:
+    from activeft import sift
+except ImportError:
+    pass
 
 __all__ = [
     "ActiveDataLoader",
